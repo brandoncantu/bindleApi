e1cb4599c4d86b753ad239749f4b81694500ecb2
Brandon Cantu <brandon.2g.05@gmail.com>
2020-12-14 18:58:43 -0600

    api test 2

--- includes/models/api_result.php
+++ includes/models/api_result.php
@@ -1,32 +1,32 @@
 <?php
-echo ('hello');
+
 // require_once('../sql_conn/config.php');
 // $resultado = $link->query($sql);
 // while($book = $resultado){
 //     echo $book;
 // }
-// header('Access-Control-Allow-Origin: *');
-// $all_books = array(
-//     'books' => array()
-// );
-// $books = array();
-// try{
-//     require_once('../sql_conn/config.php');
-//     $sql = "SELECT * FROM books";
-//     $res = $link->query($sql);
-// }catch(Exception $e){
-//     echo $e->getMessage();
-// }
-// while($book = $res->fetch_assoc() ){
-//     $respuesta = array(
-//         'id' => $book['id'],
-//         'title' => $book['title'],
-//         'img' => $book['img'],
-//         'rating' => $book['rating']
+header('Access-Control-Allow-Origin: *');
+$all_books = array(
+    'books' => array()
+);
+$books = array();
+try{
+    require_once('../sql_conn/config.php');
+    $sql = "SELECT * FROM books";
+    $res = $link->query($sql);
+}catch(Exception $e){
+    echo $e->getMessage();
+}
+while($book = $res->fetch_assoc() ){
+    $respuesta = array(
+        'id' => $book['id'],
+        'title' => $book['title'],
+        'img' => $book['img'],
+        'rating' => $book['rating']
 
-//     );
-//     array_push($books, $respuesta);
-// }
-// $all_books['books'] = $books;
-// echo json_encode($all_books, JSON_PRETTY_PRINT);
+    );
+    array_push($books, $respuesta);
+}
+$all_books['books'] = $books;
+echo json_encode($all_books, JSON_PRETTY_PRINT);
 ?>
