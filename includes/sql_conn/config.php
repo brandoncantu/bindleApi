4b3de6620d00eb3bae825b36b6379b4ac18fe2a7
Brandon Cantu <brandon.2g.05@gmail.com>
2020-12-14 18:51:54 -0600

    db test

--- includes/sql_conn/config.php
+++ includes/sql_conn/config.php
@@ -8,7 +8,7 @@
         if(!$link){
             die("Connection failed: " . mysqli_connect_error());
         }else{
-            //echo "Connection succesfull!";
+            echo "Connection succesfull!";
         }
         //$all = " SELECT * FROM eventos ";
         //$list = $link-> query($all);
